f0a6b9514e19428be98663a0c9a01abca3d761ec
Alberto Caminos <albeerc@gmail.com>
2025-05-09 14:13:04 -0300

    add effects

--- src/components2/Landing/Landing.jsx
+++ src/components2/Landing/Landing.jsx
@@ -4,12 +4,21 @@ import Socials from '../../components/Socials/Socials'
 function Landing() {
 
     return (
-        <section className="landing container" id='home'>
-            <h1>Desarrollo tu <span>sitio web</span></h1>
-            <h3>Creo sitios rápidos, claros y pensados para que tus clientes te encuentren y te contacten sin complicaciones</h3>
-            <a href="#projects"><button>Ver proyectos</button></a>
-            <Socials />
-        </section>
+            <section className="landing container" id="home">
+                <h1 className="fade-in" style={{ animationDelay: '0s' }}>
+                    Desarrollo tu <span>sitio web</span>
+                </h1>
+                <h3 className="fade-in" style={{ animationDelay: '0.3s' }}>
+                    Creo sitios rápidos, claros y pensados para que tus clientes te encuentren y te contacten sin complicaciones
+                </h3>
+                <a href="#projects" className="fade-in" style={{ animationDelay: '0.6s' }}>
+                    <button>Ver proyectos</button>
+                </a>
+                <div className="fade-in" style={{ animationDelay: '0.9s' }}>
+                    <Socials />
+                </div>
+            </section>
+
     )
 }
 
